998751731dca2af165947892c1b469caa7138ad5
gp-pereira <pereira7346@gmail.com>
2025-03-09 18:48:25 -0300

    refactor: using Document.fragment/3 to find the selection, then positioning it

--- apps/remote_control/lib/lexical/remote_control/code_action/handlers/refactorex.ex
+++ apps/remote_control/lib/lexical/remote_control/code_action/handlers/refactorex.ex
@@ -10,33 +10,42 @@ defmodule Lexical.RemoteControl.CodeAction.Handlers.Refactorex do
 
   @impl CodeAction.Handler
   def actions(%Document{} = doc, %Range{} = range, _diagnostics) do
-    original = Document.to_string(doc)
-
-    range = update_in(range.start.line, &(&1 - 1))
-    range = update_in(range.start.character, &(&1 - 1))
-    range = update_in(range.end.line, &(&1 - 1))
-    range = update_in(range.end.character, &(&1 - 1))
-
-    case Refactorex.Parser.parse_inputs(original, range) do
-      {:ok, zipper, selection_or_line} ->
-        zipper
-        |> Refactorex.Refactor.available_refactorings(selection_or_line, true)
-        |> Enum.map(fn refactoring ->
-          CodeAction.new(
-            doc.uri,
-            refactoring.title,
-            map_kind(refactoring.kind),
-            Changes.new(doc, Diff.diff(doc, refactoring.refactored))
-          )
-        end)
-
-      {:error, :parse_error} ->
-        []
+    with {:ok, target} <- line_or_selection(doc, range),
+         # Could use AST.from/1 but it would lose comments inside Refactorex
+         {:ok, ast} <- Sourceror.parse_string(Document.to_string(doc)) do
+      ast
+      |> Sourceror.Zipper.zip()
+      |> Refactorex.Refactor.available_refactorings(target, true)
+      |> Enum.map(fn refactoring ->
+        CodeAction.new(
+          doc.uri,
+          refactoring.title,
+          map_kind(refactoring.kind),
+          Changes.new(doc, Diff.diff(doc, refactoring.refactored))
+        )
+      end)
+    else
+      _ -> []
     end
   end
 
   @impl CodeAction.Handler
   def kinds, do: [:refactor]
+
+  defp line_or_selection(_, %{start: start, end: start}), do: {:ok, start.line}
+
+  defp line_or_selection(doc, %{start: start} = range) do
+    [
+      # new lines before the selection
+      String.duplicate("\n", start.line - 1),
+      # same line whitespace before the selection
+      String.duplicate(" ", start.character - 1),
+      # the selection
+      Document.fragment(doc, range.start, range.end)
+    ]
+    |> IO.iodata_to_binary()
+    |> Sourceror.parse_string()
+  end
 
   defp map_kind("quickfix"), do: :quick_fix
   defp map_kind(kind), do: :"#{String.replace(kind, ".", "_")}"
