e63a4c7504a4452a74c543933a685bead0623724
Zoey Pessanha <zoey.spessanha@zeetech.io>
2025-08-30 01:05:01 -0300

    refactor: allow burrito/multiple app nix build

--- flake.nix
+++ flake.nix
@@ -3,72 +3,91 @@
 
   inputs.nixpkgs.url = "flake:nixpkgs";
   inputs.beam-flakes.url = "github:elixir-tools/nix-beam-flakes";
-  inputs.beam-flakes.inputs.flake-parts.follows = "flake-parts";
-
-  inputs.flake-parts.url = "github:hercules-ci/flake-parts";
-  inputs.systems.url = "github:nix-systems/default";
 
   outputs = {
     self,
-    systems,
+    nixpkgs,
     beam-flakes,
     ...
-  } @ inputs:
-    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
-      imports = [beam-flakes.flakeModule];
-      flake = {
-        lib = {
-          mkExpert = {erlang}: erlang.callPackage ./nix/expert.nix {};
-        };
-      };
+  }: let
+    inherit (nixpkgs.lib) genAttrs;
+    inherit (nixpkgs.lib.systems) flakeExposed;
 
-      systems = import systems;
+    forAllSystems = f:
+      genAttrs flakeExposed (
+        system: let
+          pkgs = import nixpkgs {inherit system;};
+        in
+          f pkgs
+      );
+  in {
+    imports = [beam-flakes.flakeModule];
 
-      perSystem = {pkgs, ...}: let
-        erlang = pkgs.beam.packages.erlang_25;
-        expert = self.lib.mkExpert {inherit erlang;};
-      in {
-        formatter = pkgs.alejandra;
-
-        apps.update-hash = let
-          script = pkgs.writeShellApplication {
-            name = "update-hash";
-
-            runtimeInputs = [pkgs.nixFlakes pkgs.gawk];
-
-            text = ''
-              nix --extra-experimental-features 'nix-command flakes' \
-                build --no-link "${self}#__fodHashGen" 2>&1 | gawk '/got:/ { print $2 }' || true
-            '';
-          };
-        in {
-          type = "app";
-          program = "${script}/bin/update-hash";
-        };
-
-        packages = {
-          inherit expert;
-          default = expert;
-
-          __fodHashGen = expert.mixFodDeps.overrideAttrs (final: curr: {
-            outputHash = pkgs.lib.fakeSha256;
-          });
-        };
-        beamWorkspace = {
-          enable = true;
-          devShell.languageServers.elixir = false;
-          devShell.languageServers.erlang = false;
-          versions = {
-            elixir = "1.17.3";
-            erlang = "27.3.4.1";
-          };
-          devShell.extraPackages = with pkgs; [
-            zig
-            xz
-            just
-            _7zz
-          ];
-        };
-      };
+    lib = {
+      mkExpert = {
+        erlang,
+        system,
+        elixir ? erlang.elixir,
+        hash ? builtins.readFile ./nix/hash,
+      }:
+        erlang.callPackage ./nix/expert.nix {inherit elixir hash;};
     };
+
+    formatter = forAllSystems ({alejandra}: alejandra);
+
+    apps.update-hash = forAllSystems (pkgs: let
+      script = pkgs.writeShellApplication {
+        name = "update-hash";
+
+        runtimeInputs = [pkgs.nixFlakes pkgs.gawk];
+
+        text = ''
+          nix --extra-experimental-features 'nix-command flakes' \
+            build --no-link "${self}#__fodHashGen" 2>&1 | gawk '/got:/ { print $2 }' || true
+        '';
+      };
+    in {
+      type = "app";
+      program = "${script}/bin/update-hash";
+    });
+
+    packages = forAllSystems (pkgs: let
+      # burrito doesnt have newest OTP26 releases version
+      erlang_26 = pkgs.beam.interpreters.erlang.override rec {
+        version = "26.2.5.9";
+        src = pkgs.fetchFromGitHub {
+          owner = "erlang";
+          repo = "otp";
+          rev = "OTP-${version}";
+          sha256 = "sha256-FRNVmaeBUCFLmfhE9JVb1DgC/MIoryDV7lvh+YayRNA=";
+        };
+      };
+      expert = self.lib.mkExpert {
+        erlang = pkgs.beam.packagesWith erlang_26;
+        system = pkgs.system;
+      };
+    in {
+      inherit expert;
+      default = expert;
+
+      __fodHashGen = expert.mixFodDeps.overrideAttrs (final: curr: {
+        outputHash = pkgs.lib.fakeSha256;
+      });
+    });
+    beamWorkspace = forAllSystems (pkgs: {
+      enable = true;
+      devShell.languageServers.elixir = false;
+      devShell.languageServers.erlang = false;
+      versions = {
+        elixir = "1.17.3";
+        erlang = "27.3.4.1";
+      };
+      devShell.extraPackages = with pkgs; [
+        zig
+        xz
+        just
+        _7zz
+      ];
+    });
+  };
 }
--- nix/expert.nix
+++ nix/expert.nix
@@ -1,43 +1,152 @@
 {
+  lib,
   mixRelease,
   fetchMixDeps,
   elixir,
+  hash ? builtins.readFile ./hash,
   writeScript,
+  makeWrapper,
+  git,
+  cacert,
+  zig,
+  xz,
+  _7zz,
+  system,
 }:
 mixRelease rec {
   pname = "expert";
   version = "development";
 
-  src = ./..;
+  src = lib.cleanSource ./..;
 
   mixFodDeps = fetchMixDeps {
-    inherit pname;
-    inherit version;
+    pname = "mix-deps-${pname}";
+    inherit src version hash;
+    mixEnv = "prod";
 
-    src = ./..;
+    # Fix SSL issues for git dependencies
+    nativeBuildInputs = [git cacert];
+    GIT_SSL_CAINFO = "${cacert}/etc/ssl/certs/ca-bundle.crt";
+    SSL_CERT_FILE = "${cacert}/etc/ssl/certs/ca-bundle.crt";
 
-    sha256 = builtins.readFile ./hash;
+    installPhase = ''
+      runHook preInstall
+      cd apps/expert; mix deps.get ''${MIX_ENV:+--only $MIX_ENV}; cd -;
+      find "$TEMPDIR/deps" -path '*/.git/*' -a ! -name HEAD -exec rm -rf {} +
+      cp -r --no-preserve=mode,ownership,timestamps $TEMPDIR/deps $out
+      runHook postInstall
+    '';
   };
+
+  nativeBuildInputs = [
+    makeWrapper
+    git
+    cacert
+    zig
+    xz
+    _7zz
+  ];
+
+  MIX_ENV = "prod";
+  EXPERT_RELEASE_MODE = "burrito";
+
+  # SSL certificates for git operations
+  # GIT_SSL_CAINFO = "${cacert}/etc/ssl/certs/ca-bundle.crt";
+  # SSL_CERT_FILE = "${cacert}/etc/ssl/certs/ca-bundle.crt";
+
+  # Simple configurePhase - work from expert app and let Mix handle everything
+  configurePhase = ''
+    runHook preConfigure
+    export MIX_HOME=$TEMPDIR/.mix
+    export HEX_HOME=$TEMPDIR/.hex
+    cd apps/expert
+    mix deps.compile --no-deps-check
+    runHook postConfigure
+  '';
+
+  buildPhase = let
+    burritoTarget =
+      {
+        "aarch64-darwin" = "darwin_arm64";
+        "x86_64-darwin" = "darwin_amd64";
+        "aarch64-linux" = "linux_arm64";
+        "x86_64-linux" = "linux_amd64";
+      }."${system}" or (throw "Unsupported system: ${system}");
+  in ''
+    runHook preBuild
+    export BURRITO_TARGET=${burritoTarget}
+    export HOME=$TMPDIR/home
+    export XDG_CACHE_HOME=$TMPDIR/cache
+    mkdir -p $HOME $XDG_CACHE_HOME
+    ln -sf ../../deps ../engine/deps
+    ln -sf ../../deps ../forge/deps
+    mix compile --no-deps-check
+    mix release expert --overwrite --path ./release_build
+    runHook postBuild
+  '';
 
   installPhase = ''
     runHook preInstall
+    mkdir -p $out
+    cp -r release_build/* $out/
 
-    mix do compile --no-deps-check, package --path "$out"
+    # Handle burrito output if it exists
+    if [ -d "burrito_out" ]; then
+      mkdir -p $out/burrito_out
+      cp -r burrito_out/* $out/burrito_out/
+    fi
+
+    find $out -type l -exec test ! -e {} \; -delete
 
     runHook postInstall
   '';
 
   preFixup = let
     activate_version_manager = writeScript "activate_version_manager.sh" ''
-    true
+      #!/usr/bin/env bash
+      true
     '';
+
+    burritoBinary =
+      {
+        "aarch64-darwin" = "expert_darwin_arm64";
+        "x86_64-darwin" = "expert_darwin_amd64";
+        "aarch64-linux" = "expert_linux_arm64";
+        "x86_64-linux" = "expert_linux_amd64";
+      }."${system}" or null;
   in ''
-    substituteInPlace "$out/bin/start_expert.sh" --replace 'elixir_command=' 'elixir_command="${elixir}/bin/"'
-    rm "$out/bin/activate_version_manager.sh"
-    ln -s ${activate_version_manager} "$out/bin/activate_version_manager.sh"
+    # Fix paths in start script
+    if [ -f "$out/bin/start_expert.sh" ]; then
+      substituteInPlace "$out/bin/start_expert.sh" \
+        --replace 'elixir_command=' 'elixir_command="${elixir}/bin/"'
 
-    mv "$out/bin" "$out/binsh"
+      rm -f "$out/bin/activate_version_manager.sh"
+      ln -s ${activate_version_manager} "$out/bin/activate_version_manager.sh"
 
-    makeWrapper "$out/binsh/start_expert.sh" "$out/bin/expert" --set RELEASE_COOKIE expert
+      mv "$out/bin" "$out/binsh"
+      makeWrapper "$out/binsh/start_expert.sh" "$out/bin/expert" \
+        --set RELEASE_COOKIE expert
+    fi
+
+    # Use Burrito binary if it exists for this system
+    if [ -n "${burritoBinary}" ] && [ -f "$out/burrito_out/${burritoBinary}" ]; then
+      mkdir -p $out/bin
+      cp "$out/burrito_out/${burritoBinary}" "$out/bin/expert"
+      chmod +x "$out/bin/expert"
+    fi
+
+    # Copy Erlang cookie so distributed mode works
+    if [ -f "$out/releases/COOKIE" ]; then
+      mkdir -p $out/var
+      cp "$out/releases/COOKIE" "$out/var/COOKIE"
+      chmod 600 "$out/var/COOKIE"
+    fi
   '';
+
+  # meta = with lib; {
+  #   description = "Expert - Elixir Language Server";
+  #   homepage = "https://github.com/elixir-lang/expert";
+  #   license = licenses.mit;
+  #   platforms = platforms.unix;
+  # };
 }
