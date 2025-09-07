a92dfab979edd95af897a8f4defcd4e9ba689616
Mitchell Hanberg <mitch@mitchellhanberg.com>
2025-09-06 17:23:32 -0700

    wip

--- apps/expert/config/config.exs
+++ apps/expert/config/config.exs
@@ -5,7 +5,7 @@ config :snowflake,
   # First second of 2024
   epoch: 1_704_070_800_000
 
-case System.get_env("EXPERT_RELEASE_MODE", "plain") do
+case dbg(System.get_env("EXPERT_RELEASE_MODE", "plain")) do
   "burrito" ->
     config :expert, arg_parser: {Burrito.Util.Args, :get_arguments, []}
 
--- apps/expert/lib/expert/application.ex
+++ apps/expert/lib/expert/application.ex
@@ -12,13 +12,70 @@ defmodule Expert.Application do
 
   @impl true
   def start(_type, _args) do
+    {m, f, a} = Application.get_env(dbg(:expert), :arg_parser)
+
+
+
+
+    argv = apply(m, f, a)
+
+    {opts, _, _invalid} =
+      OptionParser.parse(argv,
+
+        strict: [version: :boolean, help: :boolean, stdio: :boolean, port: :integer]
+      )
+
+    help_text = """
+    Expert v#{Expert.vsn()}
+
+    The official language server for the Elixir programming language
+
+    expert [flags]
+
+    #{IO.ANSI.bright()}FLAGS#{IO.ANSI.reset()}
+
+      --stdio             Use stdio as the transport mechanism
+      --port <port>       Use TCP as the transport mechanism, with the given port
+      --help              Show help
+      --version           Show expert version
+    """
+
+    cond do
+      opts[:help] ->
+        IO.puts(help_text)
+
+        System.halt(0)
+
+      opts[:version] ->
+        IO.puts("#{Expert.vsn()}")
+        System.halt(0)
+
+      true ->
+        :noop
+    end
+
+    buffer_opts =
+      cond do
+        opts[:stdio] ->
+          []
+
+        is_integer(opts[:port]) ->
+          IO.puts("Starting on port #{opts[:port]}")
+          [communication: {GenLSP.Communication.TCP, [port: opts[:port]]}]
+
+        true ->
+          IO.puts(help_text)
+
+          System.halt(1)
+      end
+
     children = [
       document_store_child_spec(),
       {DynamicSupervisor, Expert.Project.DynamicSupervisor.options()},
       {DynamicSupervisor, name: Expert.DynamicSupervisor},
       {GenLSP.Assigns, [name: Expert.Assigns]},
       {Task.Supervisor, name: :expert_task_queue},
-      {GenLSP.Buffer, name: Expert.Buffer},
+      {GenLSP.Buffer, [name: Expert.Buffer] ++ buffer_opts},
       {Expert,
        name: Expert,
        buffer: Expert.Buffer,
