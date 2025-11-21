055e332c802fcc979af43da040dcb69895456aba
doorgan <dorgandash@gmail.com>
2025-11-21 19:28:07 -0300

    refactor: use quote + base64 to pass eval string
    This is the same method Livebook uses for the very same reasons

--- apps/expert/lib/expert/engine_node.ex
+++ apps/expert/lib/expert/engine_node.ex
@@ -40,20 +40,8 @@ defmodule Expert.EngineNode do
             state.cookie,
             "--no-halt",
             "-e",
-            # We manually start distribution here instead of using --sname/--name
-            # because those options are not really compatible with `-epmd_module`.
-            # Apparently, passing the --name/-sname options causes the Erlang VM
-            # to start distribution right away before the modules in the code path
-            # are loaded, and it will crash because Forge.EPMD doesn't exist yet.
-            # If we start distribution manually after all the code is loaded,
-            # everything works fine.
-            # We also need to pass the command as a single line with ;\, otherwise
-            # this will only run the first line on Windows.
-            """
-            {:ok, _} = Node.start(:"#{Project.node_name(state.project)}", :longnames);\
-            #{Forge.NodePortMapper}.register();\
-            IO.puts(\"ok\");\
-            """
+            "System.argv() |> hd() |> Base.decode64!() |> Code.eval_string()",
+            project_node_eval_string(state.project)
           ]
 
       env =
@@ -72,6 +60,29 @@ defmodule Expert.EngineNode do
           state = %{state | port: port, started_by: from}
           {:ok, state}
       end
+    end
+
+    defp project_node_eval_string(project) do
+      # We pass the child node code as --eval argument. Windows handles
+      # escaped quotes and newlines differently from Unix, so to avoid
+      # those kind of issues, we encode the string in base 64 and pass
+      # as positional argument. Then, we use a simple --eval that decodes
+      # and evaluates the string.
+      project_node = Project.node_name(project)
+
+      code =
+        quote do
+          node = unquote(project_node)
+
+          # We start distribution here, rather than on node boot, so that
+          # -pa takes effect and Forge.EPMD is available
+          {:ok, _} = Node.start(node, :longnames)
+          Forge.NodePortMapper.register()
+        end
+
+      code
+      |> Macro.to_string()
+      |> Base.encode64()
     end
 
     def stop(%__MODULE__{} = state, from, stop_timeout) do
