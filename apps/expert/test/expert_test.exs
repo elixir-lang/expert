b879ac169ffef4597327d0b86e36bf4b90d44369
Amadeus Folego <amadeusfolego@gmail.com>
2025-12-09 17:04:29 -0300

    Remove full server testing, too problematic for now

--- apps/expert/test/expert_test.exs
+++ apps/expert/test/expert_test.exs
@@ -1,82 +1,8 @@
 defmodule Expert.ExpertTest do
-  alias Forge.Test.Fixtures
-
   use ExUnit.Case, async: false
-  use Forge.Test.EventualAssertions
   use Patch
 
-  require GenLSP.Test
-
   import Expert.Test.Protocol.TransportSupport
-
-  describe "server testing" do
-    defp buffer_opts do
-      [communication: {GenLSP.Communication.TCP, [port: 0]}]
-    end
-
-    defp start_application_children do
-      pids =
-        for child_spec <- Expert.Application.children(buffer: buffer_opts()) do
-          start_supervised!(child_spec)
-        end
-
-      on_exit(fn ->
-        # NOTE: The test hangs for some reason without manually exiting
-        for pid <- pids do
-          Process.exit(pid, :normal)
-        end
-      end)
-    end
-
-    setup do
-      start_application_children()
-
-      comm_state = GenLSP.Buffer.comm_state(Expert.Buffer)
-
-      {:ok, port} = :inet.port(comm_state.lsocket)
-      %{lsp: lsp} = :sys.get_state(Expert.Buffer)
-
-      expert = %{lsp: lsp, port: port}
-      client = GenLSP.Test.client(expert)
-
-      [expert: expert, client: client]
-    end
-
-    test "replies to initialize with expert info and capabilities", %{client: client} do
-      id = System.unique_integer([:positive])
-
-      project = Fixtures.project()
-
-      root_uri = project.root_uri
-      root_path = Forge.Project.root_path(project)
-
-      assert :ok ==
-               GenLSP.Test.request(client, %{
-                 "id" => id,
-                 "jsonrpc" => "2.0",
-                 "method" => "initialize",
-                 "params" => %{
-                   "rootPath" => root_path,
-                   "rootUri" => root_uri,
-                   "capabilities" => %{},
-                   "workspaceFolders" => [
-                     %{
-                       uri: root_uri,
-                       name: root_path
-                     }
-                   ]
-                 }
-               })
-
-      GenLSP.Test.assert_result(^id, result, 500)
-
-      {:ok, initialize_result} =
-        GenLSP.Requests.Initialize.result()
-        |> Schematic.dump(Expert.State.initialize_result())
-
-      assert result == initialize_result
-    end
-  end
 
   test "sends an error message on engine initialization error" do
     with_patched_transport()
