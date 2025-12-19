738a9a6f52632455615a8df68f9f3206dbb77cbf
Mitchell Hanberg <mitch@mitchellhanberg.com>
2025-12-19 13:54:56 -0500

    Update start_expert.bat
    Co-authored-by: Dorgan <dorgandash@gmail.com>

--- apps/expert/rel/overlays/bin/start_expert.bat
+++ apps/expert/rel/overlays/bin/start_expert.bat
@@ -1,4 +1,4 @@
 @echo off
 
-"%~dp0plain.bat" eval "System.no_halt(true); Application.ensure_all_started(:xp_expert)" --stdio
+"%~dp0plain.bat" eval "System.no_halt(true); Application.ensure_all_started(:xp_expert)" %*
 
