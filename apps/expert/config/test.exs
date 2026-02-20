7f770d91c69c803c605886b66f2a5109486f7dc2
Dorgan <dorgandash@gmail.com>
2026-02-20 10:33:35 -0300

    chore: typo
    Co-authored-by: Mitchell Hanberg <mitch@mitchellhanberg.com>

--- apps/expert/config/test.exs
+++ apps/expert/config/test.exs
@@ -1,5 +1,5 @@
 import Config
 
-# Withuot this option, ExUnit would randomly not terminate or tests would behave
+# Without this option, ExUnit would randomly not terminate or tests would behave
 # erratically.
 config :gen_lsp, :exit_on_end, false
