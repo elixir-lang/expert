196840622de166c428315429d8281b0a1d26b3b0
Steve Cohen <steve.cohen@whatnot.com>
2026-03-12 17:19:14 -0700

    chore:engine bump schema to ensure valid index
    If someone had been using one of the pre-release versions of expert,
    it's likely they have an index that contains duplicate IDs.
    
    Buming the schema and making the migration return no entries causes
    expert to re-index their project, ensuring their index is valid.

---
+++ apps/engine/lib/engine/search/store/backends/ets/schemas/v4.ex
@@ -0,0 +1,34 @@
+defmodule Engine.Search.Store.Backends.Ets.Schemas.V4 do
+  @moduledoc """
+  V4 of the schema is the same as v3, but some releases had duplicate
+  IDs. This schema just causes a reindex so we remove them entirely
+  """
+  alias Engine.Search.Store.Backends.Ets.Schema
+  alias Engine.Search.Store.Backends.Ets.Schemas.V3
+
+  alias Forge.Search.Indexer.Entry
+
+  require Entry
+  use Schema, version: 4
+
+  defkey :by_id, [:id, :type, :subtype]
+
+  defkey :by_subject, [
+    :subject,
+    :type,
+    :subtype,
+    :path
+  ]
+
+  defkey :by_path, [:path]
+  defkey :by_block_id, [:block_id, :path]
+  defkey :structure, [:path]
+
+  defdelegate to_rows(entry), to: V3
+  defdelegate table_options(), to: V3
+  defdelegate to_subject(subject), to: V3
+
+  def migrate(_) do
+    {:ok, []}
+  end
+end
