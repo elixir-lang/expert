72a2f9e18f2d82986206e77369e410dd516cfe2b
Dorgan <dorgandash@gmail.com>
2026-06-18 14:02:36 -0300

    fix(engine): increase build timeout duration, add batch uri reindex with debounce (#733)
    Backport #698
    
    Co-authored-by: Christian Guetnga <33656776+lsdrfrx@users.noreply.github.com>

--- apps/engine/test/engine/commands/reindex_test.exs
+++ apps/engine/test/engine/commands/reindex_test.exs
@@ -10,34 +10,64 @@ defmodule Engine.Commands.ReindexTest do
   alias Engine.Search
   alias Forge.Document
 
-  setup do
+  setup context do
+    debounce_interval_millis = Map.get(context, :debounce_interval_millis, 0)
+    test_pid = self()
+
     reindex_fun = fn _ ->
-      Process.sleep(20)
+      test_ref = Process.monitor(test_pid)
+      send(test_pid, {:reindex_started, self()})
+
+      receive do
+        :complete_reindex ->
+          Process.demonitor(test_ref, [:flush])
+
+        {:DOWN, ^test_ref, :process, ^test_pid, _reason} ->
+          :ok
+      end
     end
 
-    start_supervised!({Reindex, reindex_fun: reindex_fun})
+    start_supervised!(
+      {Reindex, reindex_fun: reindex_fun, debounce_interval_millis: debounce_interval_millis}
+    )
 
     {:ok, project: project()}
   end
 
   test "it should allow reindexing", %{project: project} do
     assert :ok = Reindex.perform(project)
+    reindex_pid = await_reindex_started()
     assert Reindex.running?()
+    complete_reindex(reindex_pid)
   end
 
   test "it fails if another index is running", %{project: project} do
     assert :ok = Reindex.perform(project)
+    reindex_pid = await_reindex_started()
     assert {:error, "Already Running"} = Reindex.perform(project)
+    complete_reindex(reindex_pid)
   end
 
   test "it eventually becomes available", %{project: project} do
     assert :ok = Reindex.perform(project)
+    await_reindex_started() |> complete_reindex()
     refute_eventually Reindex.running?()
   end
 
   test "another reindex can be enqueued", %{project: project} do
     assert :ok = Reindex.perform(project)
+    await_reindex_started() |> complete_reindex()
     assert_eventually :ok = Reindex.perform(project)
+    await_reindex_started() |> complete_reindex()
+  end
+
+  defp await_reindex_started do
+    assert_receive {:reindex_started, reindex_pid}
+    reindex_pid
+  end
+
+  defp complete_reindex(reindex_pid) do
+    send(reindex_pid, :complete_reindex)
   end
 
   def put_entries(uri, entries) do
@@ -80,8 +110,11 @@ defmodule Engine.Commands.ReindexTest do
       uri = "file:///file.ex"
       new_entries = [reference(), definition()]
       put_entries(uri, new_entries)
-      Reindex.perform(project)
+      assert :ok = Reindex.perform(project)
+      reindex_pid = await_reindex_started()
+
       Reindex.uri(uri)
+      complete_reindex(reindex_pid)
 
       assert_receive {:entries, "/file.ex", ^new_entries}
     end
